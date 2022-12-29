75614137c887daa230ecb8ec79652ab3da636ab2
Kacper Zurawski <kaczur190@gmail.com>
2022-12-29 07:30:36 +0100

    bug with DOM fixed

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
-    var block = document.getElementById("block");
-    var gap = document.getElementById("gap");
 
-    gap.addEventListener('animationiteration', () => {
-        var random = -((Math.random() * 400) + 150);
-        gap.style.top = random + "px";
-    })
+
 });
