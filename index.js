99158f7d3aa2fc84eeb0d5e5318f25d41349c86e
Kacper Zurawski <kaczur190@gmail.com>
2022-12-29 08:30:30 +0100

    random obstacles added

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
 
+    var block = document.getElementById("block");
+    var gap = document.getElementById("gap");
 
+    gap.addEventListener("animationiteration", () => {
+        var random = - ((Math.random() * 400) + 200)
+        gap.style.top = random + "px";
+    })
 });
