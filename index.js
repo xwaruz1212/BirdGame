9a6bfa1affb5bbf95f890f51bc0b661b59416ac4
Kacper Zurawski <kaczur190@gmail.com>
2022-12-29 09:05:04 +0100

    gravity function added

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
     var block = document.getElementById("block");
     var gap = document.getElementById("gap");
+    var character = document.getElementById("character")
 
     gap.addEventListener("animationiteration", () => {
-        var random = - ((Math.random() * 400) + 200)
+        var random = - ((Math.random() * 400) + 150);
         gap.style.top = random + "px";
     })
+
+    setInterval(function () {
+        var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+        character.style.top = (characterTop + 3) + "px";
+        console.log(characterTop)
+    }, 20)
+
 });
