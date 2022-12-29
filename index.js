67236cc7bc52f217446b9b261b3c0519838fa1f1
Kacper Zurawski <kaczur190@gmail.com>
2022-12-29 10:05:01 +0100

    flying function added

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
     var gap = document.getElementById("gap");
     var character = document.getElementById("character")
 
+    var flying = 0;
+
     gap.addEventListener("animationiteration", () => {
         var random = - ((Math.random() * 400) + 150);
         gap.style.top = random + "px";
@@ -13,7 +15,30 @@ window.addEventListener('DOMContentLoaded', (event) => {
     setInterval(function () {
         var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
         character.style.top = (characterTop + 3) + "px";
-        console.log(characterTop)
-    }, 20)
+        //console.log(characterTop)
+    }, 10);
 
+    function fly() {
+        flying = 1;
+        let flyCount = 0;
+
+        var flyInterval = setInterval(() => {
+            var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+            if ((characterTop > 10) && (flyCount < 20)) {
+                character.style.top = (characterTop - 7) + "px"
+            }
+            if (flyCount > 20) {
+                clearInterval(flyInterval);
+                flying = 0;
+                flyCount = 0;
+            }
+            flyCount++
+        }, 30);
+    }
+
+    document.addEventListener('keyup', event => {
+        if (event.code === 'Space') {
+            fly()
+        }
+    })
 });
